7706d7d558f77cc586f9232ea2aa4ac380601c76
wearevoyager <georgepaterson@wearevoyager.com>
2021-08-27 23:59:41 +0100

    Working out a new component layout.

---
+++ src/components/footer.js
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+const navigation = {
+  main: [
+    { name: 'Home', href: '#' },
+    { name: 'Platform', href: '#' }
+  ]
+}
+
+const Footer = () => {
+  return (
+    <footer className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+          {navigation.main.map((item) => (
+            <div key={item.name} className="px-5 py-2">
+              <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                {item.name}
+              </a>
+            </div>
+          ))}
+        </nav>
+        <p className="mt-8 text-center text-base text-gray-400">&copy; 2021 We are Voyager Limited. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
